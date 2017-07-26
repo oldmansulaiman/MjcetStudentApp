941209a198e0649e7364fea78085e4655eb18aba
oldmansulaiman <sulaiman2199.saq@gmail.com>
2017-07-26 23:29:52 +0530

    Initial commit

--- app/src/main/java/com/example/admin/mjcetstudentapp/MainActivity.java
+++ app/src/main/java/com/example/admin/mjcetstudentapp/MainActivity.java
@@ -1,13 +1,148 @@
 package com.example.admin.mjcetstudentapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.navigation_drawer);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() == null) {
+            Intent loginActivityIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginActivityIntent);
+            finish();
+        }
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer);
+        drawerLayout.setDrawerListener(toggle);
+
+        toggle.syncState();
     }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        }else {
+
+            super.onBackPressed();
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id==R.id.item1_id){
+
+            firebaseAuth.signOut();
+            Intent loginActivityIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginActivityIntent);
+            finish();
+
+        }else if (id==android.R.id.home){
+
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+
+            case R.id.attendance_id:
+
+                Toast.makeText(getApplicationContext(),"attendance",Toast.LENGTH_LONG).show();
+
+                break;
+
+            case R.id.marks_id:
+
+                Toast.makeText(getApplicationContext(),"marks",Toast.LENGTH_LONG).show();
+
+                break;
+
+            case R.id.student_activities_id:
+
+                Toast.makeText(getApplicationContext(),"student activities",Toast.LENGTH_LONG).show();
+
+                break;
+
+            case R.id.vision_id:
+
+                Toast.makeText(getApplicationContext(),"vision",Toast.LENGTH_LONG).show();
+
+                break;
+
+            case R.id.mission_id:
+
+                Toast.makeText(getApplicationContext(),"mission",Toast.LENGTH_LONG).show();
+
+                break;
+
+            case R.id.about_us_id:
+
+                Intent intent = new Intent(this,AboutActivity.class);
+                startActivity(intent);
+
+            break;//
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+
+
 }
